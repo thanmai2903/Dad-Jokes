61783a4c9ed23bd5ea1db02df77ed4dd1865d4b5
thanmai2903 <thanmaipalla329@gmail.com>
2024-12-05 19:44:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const jokeE1 = document.getElementById("joke");
+const jokebtn = document.getElementById("jokebtn");
+jokebtn.addEventListener("click", generateJoke);
+generateJoke();
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+  const res = await fetch("https://icanhazdadjoke.com/", config);
+  const data = await res.json();
+  jokeE1.innerHTML = data.joke;
+}
